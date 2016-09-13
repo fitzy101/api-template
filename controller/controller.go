07af233448425ae3d2d6bf5fdf06126f173d4b55
Michael Fitz-Payne <mfitz-payne@wolas.com.au>
2016-09-13 19:32:05 +1000

    Updated several names to be in line with Go standard conventions. Removed unnecessary function to setup db in controller layer.

--- controller/controller.go
+++ controller/controller.go
@@ -3,19 +3,7 @@ package controller
 import (
 	"fmt"
 	"net/http"
-
-	"github.com/jmoiron/sqlx"
 )
-
-var (
-	db *sqlx.DB
-)
-
-// Set the database instance to the database connected to.
-func SetDB(conn *sqlx.DB) {
-	db = conn
-	return
-}
 
 // StatusCheck is an unauthenticated route indicating the API has booted and
 // has successfully established a DB connection.
